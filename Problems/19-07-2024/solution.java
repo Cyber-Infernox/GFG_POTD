04bfa9923960410161b20149fbf4a6682fe480e9
Cyber_Infernox <sayonkar@gmail.com>
2024-07-19 21:06:09 +0530

    Update solution.java

--- Problems/19-07-2024/solution.java
+++ Problems/19-07-2024/solution.java
@@ -1,34 +1,46 @@
 class Solution {
-    public int binarySearch(List<Integer> v, int x) {
-        int l = 0, h = v.size() - 1, mid;
-        while (l <= h) {
-            mid = (l + h) / 2;
-            if (v.get(mid) == x)
-                return mid;
-            else if (v.get(mid) > x)
-                h = mid - 1;
+    int binarySearch(ArrayList<Integer> list, int value) {
+        int low = 0;
+        int high = list.size() - 1;
+        
+        while(low <= high) {
+            int mid = (low + high) / 2;
+            
+            if(list.get(mid) == value)
+            return mid;
+            
+            else if(list.get(mid) > value)
+            high = mid - 1;
+            
             else
-                l = mid + 1;
+            low = mid + 1;
         }
+        
         return -1;
     }
-
-    public int[] constructLowerArray(int[] arr) {
-        List<Integer> v = new ArrayList<>();
-        for (int num : arr) {
-            v.add(num);
+    
+    int[] constructLowerArray(int[] arr) {
+        // code here
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i = 0; i < arr.length; i++) {
+            list.add(arr[i]);
         }
-        Collections.sort(v);
-
-        int[] t = new int[arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            int max = binarySearch(v, arr[i]);
-            while (max > 0 && v.get(max - 1).equals(v.get(max))) {
-                max--;
+        
+        Collections.sort(list);
+        
+        for(int i = 0; i < arr.length; i++) {
+            int value = binarySearch(list, arr[i]);
+            
+            while (value > 0 && list.get(value - 1).equals(list.get(value))) {
+                value--;
             }
-            t[i] = max;
-            v.remove(max);
+            
+            arr[i] = value;
+            
+            list.remove(value);
         }
-        return t;
+        
+        return arr;
     }
 }
